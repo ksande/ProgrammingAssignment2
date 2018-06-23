fe53693b672143f7cd819cf8b0cab81f5bf19c36
Sandeep Kumar <show2001@yahoo.com>
2018-06-22 22:25:10 -0400

    Implemented the 2 assignment function makeCacheMatrix & cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function return a list of 4 untility functions to cache a matrix
+## The matrix to be cached can be any matrix that is precomputed
 
-## Write a short comment describing this function
+## This function has a list of 4 utility function
+## get -- retrieve original input matrix
+## set -- overwrite with a new input matrix
+## getMatrix -- retrieve the computed matrix
+## setMatrix -- set computed Matrix in the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        mat <- NULL
+        set <- function(y){
+                mat <- NULL
+                x <<- y
+        }
+        get <- function() x
+        getMatrix <- function() mat
+        setMatrix <- function(input_mat) mat <<- input_mat
+        list(set = set, get = get, getMatrix = getMatrix, setMatrix = setMatrix)
 }
 
 
-## Write a short comment describing this function
+## This function computed the matrix inverse using the function solve()
+## And store the computed matrix in cache. A subsequent retrieval of inverted matrix
+## is returned from cache
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inverted <- x$getMatrix()
+        if(!is.null(inverted)){
+                message("getting cached data")
+                return (inverted)
+        }
+        data <- x$get()
+        inverted <- solve(data)
+        x$setMatrix(inverted)
+        inverted
 }
